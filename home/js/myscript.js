4b41502750930224ccedf0c26e346f6f0bb17553
RKMerc <ramercado3@student.apc.edu.ph>
2024-12-16 14:57:49 +0800

    moved script to myscript.js

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,15 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
   }
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  alert("Congratulations!");
+});
+function mOver(obj) {
+  obj.innerHTML = "Click the button!"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Psst! Hey!"
+}
