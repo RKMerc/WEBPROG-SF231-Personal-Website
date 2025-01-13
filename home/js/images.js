466abb137b51e46511230e75a59f48fa4b4bb099
RKMerc <ramercado3@student.apc.edu.ph>
2025-01-13 15:13:33 +0800

    added script.js

--- home/js/images.js
+++ home/js/images.js
@@ -1,7 +1,7 @@
 const app2 = Vue.createApp({
     data() {
      return {
-       gallery: [
+       gallery : [
         {name: 'TNC', url: 'https://i0.wp.com/live.staticflickr.com/65535/49634716071_3d4d4dcb32_z.jpg?ssl=1'},
         {name: 'Asia Pacific College', url: 'https://www.apc.edu.ph/wp-content/uploads/2020/02/APC-Building-Aerial-Shot-web-new.jpg'},
         
