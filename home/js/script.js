c52c6be8331f9381b9720b459aa0672ad2a2d87d
RKMerc <ramercado3@student.apc.edu.ph>
2025-01-16 13:35:42 +0800

    updated index.html, script.js, styles.css

--- home/js/script.js
+++ home/js/script.js
@@ -1,3 +1,9 @@
-function hover() {
-    
-}
+const button = document.getElementById("myButton");
+
+button.onmouseover = function() {
+  this.classList.add("hovered");
+};
+
+button.onmouseout = function() {
+  this.classList.remove("hovered");
+};
